b967553b143f7652c4c1eea2671b7845d5530f06
Jihyun Heo <jihyun.heo1226@gmail.com>
2022-05-16 22:16:29 +0900

    May 16

--- week1.js
+++ week1.js
@@ -1,4 +1,33 @@
 //2022.5.16
 //1.윤년구하기
+//전제조건
+//4로 나누어떨어지는 해는 윤년이다
+//하지만 100으로 나누어떨어지는 해는 윤년이 아니다
+//하지만 400으로 나누어떨어지는 해는 윤년이다
+function isLeapYear(year) {
+  return (year % 4 === 0 && year % 100 != 0) || year % 400 === 0;
+}
+
+console.log(`2020년은 윤년인가요? ${isLeapYear(2020)}`);
 
 //2.a부터 b까지 합 구하기
+function sumAll(a, b) {
+  let output = 0;
+  for (let i = a; i <= b; i++) {
+    output += i;
+  }
+  return output;
+}
+
+console.log(sumAll(5, 19));
+
+//곱도 구해보자
+function multiflyAll(a, b) {
+  let output = 1;
+  for (let i = a; i <= b; i++) {
+    output *= i;
+  }
+  return output;
+}
+
+console.log(multiflyAll(4, 12));
