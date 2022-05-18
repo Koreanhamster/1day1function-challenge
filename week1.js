a7155c1f699fa0db7a58ac198cd647b2436ad0e2
Jihyun Heo <jihyun.heo1226@gmail.com>
2022-05-18 17:45:54 +0900

    가격낮은순부터 정렬

--- week1.js
+++ week1.js
@@ -45,4 +45,24 @@ function getMin(array) {
 }
 
 const myArray = [22, 456, 785, 23, 90];
-console.log(`${myArray}중 가장 작은 숫자는 ${getMin(myArray)}`);
+// console.log(`${myArray}중 가장 작은 숫자는 ${getMin(myArray)}`);
+
+//5.18
+//가격낮은순으로 쏠팅하기
+
+let products = [
+  { id: 0, price: 70000, title: 'Blossom Dress' },
+  { id: 1, price: 32000, title: 'Check Shirt' },
+  { id: 2, price: 15000, title: 'Black Tie' },
+  { id: 3, price: 67000, title: 'Springfield Shirt' },
+  { id: 4, price: 82000, title: 'Red Mini Dress' },
+];
+
+let lowToHigh = function () {
+  products.sort(function (a, b) {
+    return a.price - b.price;
+  });
+  console.log(products);
+};
+
+lowToHigh();
