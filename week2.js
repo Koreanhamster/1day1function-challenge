8a0e64419d12d5cabdf44f3d8c0d4a9b2be244f2
Jihyun Heo <jihyun.heo1226@gmail.com>
2022-05-23 23:48:43 +0900

    setInterval사용

---
+++ week2.js
@@ -0,0 +1,20 @@
+//5.23
+//Dom - 5초부터 1초까지 세고 사라지는 알럿창 만들기
+//<div class="alert alert-danger" id="alert">
+//<span id="time">5</span>초 이내 구매시 사은품 증정
+//</div>
+
+// const alert = document.querySelector('#alert');
+// const time = document.querySelector('#time');
+
+// let count = 5;
+
+// setInterval(function () {
+//   count -= 1;
+//   if (count >= 0) {
+//     time.textContent = count;
+//   }
+//   if (count == 0) {
+//     alert.style.display = 'none';
+//   }
+// }, 1000);
