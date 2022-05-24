a5d11206cd5dcd311cb65f602e57bd5e46abade0
Jihyun Heo <jihyun.heo1226@gmail.com>
2022-05-24 14:44:36 +0900

    정답판독기

--- week2.js
+++ week2.js
@@ -18,3 +18,29 @@
 //     alert.style.display = 'none';
 //   }
 // }, 1000);
+
+// 5.24 퀴즈판독기
+// <body>
+//   <p>태조 이성계가 태어난 연도는?</p>
+//   <input type="text" id="answer" />
+//   <button id="send-answer" type="submit">제출</button>
+
+//   <script>
+//     const answerBtn = document.querySelector('#send-answer');
+//     let count = 0;
+
+//     // 버튼을 누를때마다 카운트를 올려주세요
+//     answerBtn.addEventListener('click', function () {
+//       count++;
+//       const answer = document.querySelector('#answer').value;
+//       console.log(answer);
+//       if (answer == 1335) {
+//         alert('정답');
+//       } else if (answer != 1335 && count < 3) {
+//         alert('땡');
+//       } else if (answer != 1335 && count >= 3) {
+//         alert('멍청아!');
+//       }
+//     });
+//   </script>
+// </body>
