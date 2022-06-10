5cef143a4da75a564a4af172e269072151160092
Koreanhamster <jihyun.heo1226@gmail.com>
2022-06-11 00:44:31 +0900

    배열 요소 변경하는 함수 만들기

--- week4/week4.js
+++ week4/week4.js
@@ -10,5 +10,25 @@ function checkDay(a, b) {
   console.log(day[theDay.getDay()]);
 }
 
-checkDay(6, 7);
-checkDay(6, 8);
+// checkDay(6, 7);
+// checkDay(6, 8);
+
+// 퀴즈1: 주어진 배열 안의 딸기 아이템을 키위로 교체하는 함수를 만들기
+// 단, 주어진 배열을 수정하지 않도록!
+// input: ['🍌', '🍓', '🍇', '🍓']
+// output: [ '🍌', '🥝', '🍇', '🥝' ]
+
+const array = ['🍌', '🍓', '🍇', '🍓'];
+
+function replace(array, from, to) {
+  const newReplace = Array.from(array);
+  for (let i = 0; i <= newReplace.length; i++) {
+    if (newReplace[i] === from) {
+      newReplace[i] = to;
+    }
+  }
+  return newReplace;
+}
+
+const result = replace(array, '🍌', '🥝');
+console.log(result);
