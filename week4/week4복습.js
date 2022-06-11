04073eca2f24472d0913bdfca0b958cff28a603c
Koreanhamster <jihyun.heo1226@gmail.com>
2022-06-11 13:33:46 +0900

    week4 복습

--- week4/week4복습.js
+++ week4/week4복습.js
@@ -3,26 +3,23 @@
 
 // getDay() : 일~토 (0~6)로 표현하기 때문에 "SUN"부터 배열에 넣기
 // getMonth()도 0부터 시작
-const day = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+
+const day = ['일', '월', '화', '수', '목', '금', '토'];
 
 function checkDay(a, b) {
   const theDay = new Date(`2022-${a}-${b}`);
   console.log(day[theDay.getDay()]);
 }
 
-// checkDay(6, 7);
-// checkDay(6, 8);
-
-// 퀴즈1: 주어진 배열 안의 딸기 아이템을 키위로 교체하는 함수를 만들기
-// 단, 주어진 배열을 수정하지 않도록!
-// input: ['🍌', '🍓', '🍇', '🍓']
-// output: [ '🍌', '🥝', '🍇', '🥝' ]
+// checkDay(6, 11);
+// checkDay(6, 12);
 
 const array = ['🍌', '🍓', '🍇', '🍓'];
 
 function replace(array, from, to) {
-  const newReplace = Array.from(array);
-  for (let i = 0; i <= newReplace.length; i++) {
+  let newReplace = Array.from(array);
+
+  for (let i = 0; i <= array.length; i++) {
     if (newReplace[i] === from) {
       newReplace[i] = to;
     }
@@ -30,5 +27,4 @@ function replace(array, from, to) {
   return newReplace;
 }
 
-const result = replace(array, '🍓', '🥝');
-console.log(result);
+console.log(replace(array, '🍓', '🍌'));
